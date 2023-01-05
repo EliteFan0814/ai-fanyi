e5b0f7f4538b91614442755ddd57ee07542d34a1
fanpeichao <fan552426811@outlook.com>
2023-01-05 11:21:51 +0800

    all

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,7 @@ const program = new Command();
 program
   .name("aify")
   .version("0.0.1")
-  .description("翻译命令行工具，自动识别语言，默认翻译为中文");
+  .description("一个简单的翻译命令行工具，自动识别语言，默认翻译为中文");
 
 program
   .argument("<transFrom>", "<填写需要翻译的文字>，默认翻译为中文")
